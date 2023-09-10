ea13dcdf667e981d1b7f096a4b400ddd6a40c27a
SMFRafin <muntasinfayaz@gmail.com>
2023-09-10 21:17:12 +0600

    new file:   application.html 	modified:   jobs.html 	modified:   web.js

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const sidebar = document.getElementById('sidebar');
     const openSidebarButton = document.getElementById('openSidebarButton');
     const closeSidebarButton = document.getElementById('closeSidebarButton');
+    const detailsButtons = document.querySelectorAll('details');
 
     function display() {
         header.innerHTML = '<h1> Welcome to Virtual Job Fair</h1>';
@@ -16,9 +17,16 @@ document.addEventListener("DOMContentLoaded", function() {
     function closeSidebar() {
         sidebar.style.left = '-250px';
     }
-
+    //Function for details button
+    function toggleJobDetails(){
+        const jobDetails= this.nextElementSibling;
+        jobDetails.classlist.toggle('hidden');
+    }
     // Event listeners for buttons
     openSidebarButton.addEventListener('click', openSidebar);
     closeSidebarButton.addEventListener('click', closeSidebar);
+    detailsButtons.forEach(function(button) {
+        button.addEventListener('click', toggleJobDetails);
+    });
     display();
 });
